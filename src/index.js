d2ad2649be3a717bdfac139ca576df3e14bbaa9f
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2023-04-12 15:12:36 +0100

    save

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ require('./config/passport')(passport);
 app.get('/', (req, res) => res.send('Hello my World'));
 
 require('./routes/user.js')(app);
+require('./routes/required_doc.js')(app);
 
 //create a server
 var server = app.listen(port, function() {
