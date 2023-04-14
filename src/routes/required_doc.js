b54fc63bacbfc060264532f84ac5a92fba2e37f4
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2023-04-14 12:40:11 +0100

    save

--- src/routes/required_doc.js
+++ src/routes/required_doc.js
@@ -1,7 +1,9 @@
+const { Application_form } = require("../controllers/application");
 const { required_doc, list_doc } = require("../controllers/require_doc")
 
 
 module.exports = (app)=>{
 app.post('/api/required_docs',required_doc)
 app.post('/api/list_doc',list_doc)
+app.post("/api/Application_form", Application_form);
 }
