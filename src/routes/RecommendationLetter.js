860a188fe2049145a4fbd2fdf42646d9f657efd0
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2023-04-18 12:53:48 +0100

    save

--- src/routes/RecommendationLetter.js
+++ src/routes/RecommendationLetter.js
@@ -1,7 +1,7 @@
 import passport from 'passport';
 import config from '../config/config';
 import { allowOnly } from '../services/routesHelper';
-import {RecommendationLetter} from '../controllers/RecommendationLetter';
+import {RecommendationLetter, getAp, getList, getPolicy, getRec, layoutPolicies} from '../controllers/RecommendationLetter';
 
 
 module.exports = (app) => {
@@ -10,4 +10,9 @@ module.exports = (app) => {
         '/api/create-recommendation-letter',
         RecommendationLetter
     );
+    app.get('/api/getCadestral',getList)
+    app.post('/api/layout_policies',layoutPolicies)
+    app.get('/api/getPolicys',getPolicy)
+    app.get('/api/getAppBYID',getRec)
+    app.get('/api/getreByIDs',getAp)
 };
--- src/routes/required_doc.js
+++ src/routes/required_doc.js
@@ -1,9 +1,19 @@
-const { Application_form } = require("../controllers/application");
-const { required_doc, list_doc } = require("../controllers/require_doc")
+const { Application_form, getApplication, getByID, postFinance, getFinance, getCharges, getFiles, getF, generateFile_no, getPlots } = require("../controllers/application");
+const { required_doc, list_doc, getList } = require("../controllers/require_doc")
 
 
 module.exports = (app)=>{
 app.post('/api/required_docs',required_doc)
 app.post('/api/list_doc',list_doc)
 app.post("/api/Application_form", Application_form);
+app.get('/api/getList',getList)
+app.get('/api/get-application',getApplication);
+app.get('/api/getByID',getByID)
+app.post('/api/post-finace',postFinance);
+app.get('/api/getfinance',getFinance)
+app.get('/api/getCharges',getCharges)
+app.get('/api/generate',getFiles)
+app.get('/api/getF',getF)
+app.post('/api/generate-file-no',generateFile_no)
+app.get('/api/plots',getPlots)
 }
