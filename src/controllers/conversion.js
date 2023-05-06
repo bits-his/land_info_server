871d3f3cfb738b7e3719b95af42816f523e3f014
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2023-05-06 12:53:47 +0100

    save

--- src/controllers/conversion.js
+++ src/controllers/conversion.js
@@ -20,15 +20,16 @@ export const conversion = (req, res) => {
         date = null,
         signature = null,
         file_no = null,
+        status='pending'
     } = req.body;
     console.log(req.body);
     // const { in_query_type = null } = req.query;
     db.sequelize
         .query(
             `INSERT INTO lis.conversion(
-                 applicant_full_name, age, occupation, nationality, state_of_origin, home_domicile, add_in_nig, phone, name_add_of_lcl_rep, suff_desc, customary_title, purp_for_land_req, value_of_impr_offered_and_time_for_erection, applicants_src_of_financing_impr_offered, len_of_time_req, date, signature, file_no)
-                VALUES ('${applicant_full_name}', ${parseInt(age)}, '${occupation}', '${nationality}', '${state_of_origin}', '${home_domicile}', '${add_in_nig}', '${parseInt(phone)}', '${name_add_of_lcl_rep}', '${suff_desc}', '${customary_title}', '${purp_for_land_req}', '${value_of_impr_offered_and_time_for_erection}', '${applicants_src_of_financing_impr_offered}', '${len_of_time_req}', '${date}', '${signature}', '${file_no}')  `,
-''
+                 applicant_full_name, age, occupation, nationality, state_of_origin, home_domicile, add_in_nig, phone, name_add_of_lcl_rep, suff_desc, customary_title, purp_for_land_req, value_of_impr_offered_and_time_for_erection, applicants_src_of_financing_impr_offered, len_of_time_req, date, signature, file_no,status)
+                VALUES ('${applicant_full_name}', ${parseInt(age)}, '${occupation}', '${nationality}', '${state_of_origin}', '${home_domicile}', '${add_in_nig}', '${parseInt(phone)}', '${name_add_of_lcl_rep}', '${suff_desc}', '${customary_title}', '${purp_for_land_req}', '${value_of_impr_offered_and_time_for_erection}', '${applicants_src_of_financing_impr_offered}', '${len_of_time_req}', '${date}', '${signature}', '${file_no}','${status}')  `,
+
         )
         .then((results) => res.json({ succes: true, results }))
         .catch((err) => {
@@ -36,4 +37,14 @@ export const conversion = (req, res) => {
             res.status(500).json({ succes: false });
         });
 };
+
+export const getConversion = (req,res)=>{
+
+    db.sequelize.query(`select * from lis.conversion where status='pending'`)
+    .then((results) => res.json({ succes: true, results }))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({ succes: false });
+    });
+}
 // export { Application_form};
--- src/routes/conversion.js
+++ src/routes/conversion.js
@@ -1,5 +1,6 @@
-const { conversion } = require("../controllers/conversion")
+const { conversion, getConversion } = require("../controllers/conversion")
 module.exports = (app) => {
     app.post("/api/conversion", conversion);
+    app.get('/api/getConversion',getConversion)
     // app.post('/api/list_doc',list_doc)
 };
